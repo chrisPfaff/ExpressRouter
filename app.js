5acc925b029a9e8c21f6ff3410f6a82439290c2f
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-27 20:44:14 -0600

    refactored all the views in ejs

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-let fs = require('fs');
-let express = require('express');
-let request = require('request');
-let ejs = require('ejs');
-let methodOverride = require('method-override');
-let bodyParser = require('body-parser');
-let morgan = require('morgan');
+let fs = require("fs");
+let express = require("express");
+let request = require("request");
+let ejs = require("ejs");
+let methodOverride = require("method-override");
+let bodyParser = require("body-parser");
+let morgan = require("morgan");
 let app = express();
 let port = 3000;
-let routes = require('./routes/item');
+let routes = require("./routes/item");
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
-
-app.use(morgan('tiny'));
-app.use(express.static(__dirname + '/public'));
+app.use(morgan("tiny"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(methodOverride(function (req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}));
-app.use('/', routes);
+app.use(methodOverride("_method"));
+app.use(
+  methodOverride(function(req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+app.use("/", routes);
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
 });
 
-
-
-
-app.listen(port, function(){
-  console.log('listening on port 3000');
+app.listen(port, function() {
+  console.log("listening on port 3000");
 });
--- routes/item.js
+++ routes/item.js
@@ -1,81 +1,80 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
+router.use(express.static(__dirname + "/public"));
 
-router.use(express.static(__dirname + '/public'));
-
-let groceryList = [['Ham', 1], ['Cheese', 2]];
+let groceryList = [["Ham", 1], ["Cheese", 2]];
 let id = 3;
 
-router.get('/', function(req, res) {
-  res.render('items' , {
+router.get("/", function(req, res) {
+  res.render("items", {
     groceryListItems: groceryList
   });
 });
 
-router.get('/new', function(req, res) {
-  res.render('new');
-})
+router.get("/new", function(req, res) {
+  res.render("new");
+});
 
-router.get('/get', function(req, res) {
-  res.render('get',  {
+router.get("/get", function(req, res) {
+  res.render("get", {
     groceryListItems: groceryList
   });
-})
+});
 
-router.get('/get/:id', function(req, res) {
+router.get("/item/get/:id", function(req, res) {
   var item = req.query.item;
   groceryList.forEach(function(listItems) {
-    if(item === listItems[0]) {
-      res.render('showItem', {item: item})
+    if (item === listItems[0]) {
+      res.render("showItem", { item: item });
     }
-  })
-  res.render('notFound');
-})
+  });
+  res.render("notFound");
+});
 
-router.get('/edit', function(req, res) {
-  res.render('edit', {groceryListItems: groceryList});
-})
+router.get("/edit", function(req, res) {
+  res.render("edit", { groceryListItems: groceryList });
+});
 
-router.post('/edit/:id' , function(req, res) {
+router.post("/item/edit/:id", function(req, res) {
   var oldItem = req.body.item;
   var newItem = req.body.newItem;
+  console.log(newItem, oldItem);
   groceryList.forEach(function(listItems, index) {
-    if(oldItem === listItems[0]) {
+    if (oldItem === listItems[0]) {
       var id = listItems[1];
       console.log(newItem);
       groceryList.splice(index, 1);
       groceryList.splice(index, 0, [newItem, id]);
-      res.redirect('/item');
+      res.redirect("/");
     }
-  })
-  res.render('notFound');
-})
+  });
+  res.render("notFound");
+});
 
-router.post('/', function(req, res) {
+router.post("/item", function(req, res) {
   console.log(req.body.item);
   groceryList.push([req.body.item, id]);
   id++;
-  res.redirect('/item');
+  res.redirect("/");
 });
 
-router.get('/delete', function(req, res) {
-  res.render('delete', {
+router.get("/delete", function(req, res) {
+  res.render("delete", {
     groceryListItems: groceryList
   });
-})
+});
 
-router.delete('/delete/:id', function(req, res) {
+router.delete("/item/delete/:id", function(req, res) {
   var item = req.body.item;
+  console.log(req);
   groceryList.forEach(function(listItems, index) {
-    if(item === listItems[0]) {
+    if (item === listItems[0]) {
       groceryList.splice(index, 1);
-      res.redirect('/item');
+      res.redirect("/");
     }
-  })
-  res.render('notFound');
-})
-
-
+  });
+  res.render("notFound");
+});
 
 module.exports = router;
